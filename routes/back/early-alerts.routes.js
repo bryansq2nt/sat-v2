f1ce6a55cbb53cb81f4a28b6502854d4faa15863
Bryan Murgas <bryan_murgasvasquez@hotmail.com>
2021-05-28 12:17:16 -0600

    update and analyze

--- routes/back/early-alerts.routes.js
+++ routes/back/early-alerts.routes.js
@@ -2,15 +2,18 @@ const {Router} = require('express');
 const router = Router();
 
 const { usersTokenVerification } = require('../../middlewares/token.middleware');
-const { earlyAlertsList, getById, getEarlyAlertForm, createEarlyAlert, updateEarlyAlert } = require('@controllers/back/early-alert.controllers');
-const upload = require('../../lib/multer');
+const { earlyAlertsList, getById, getEarlyAlertForm, createEarlyAlert, updateEarlyAlert,getFormToAnalyze,analyzeEarlyAlert } = require('@controllers/back/early-alert.controllers');
 
 router.get('/api/early-alerts/list', earlyAlertsList);
 router.get('/api/early-alerts/:id_alerta_temprana', getById);
 router.put('/api/early-alerts/:id_alerta_temprana',usersTokenVerification, updateEarlyAlert);
+router.put('/api/early-alerts/:id_alerta_temprana/analyze',usersTokenVerification, analyzeEarlyAlert);
 
 router.get('/api/early-alerts-form', getEarlyAlertForm);
-router.post('/api/early-alerts-form', usersTokenVerification, upload.any(), createEarlyAlert);
+router.get('/api/early-alerts-form-to-analyze/:id_alerta_temprana', getFormToAnalyze);
+
+router.post('/api/early-alerts-form', usersTokenVerification, createEarlyAlert);
+
 
 
 
