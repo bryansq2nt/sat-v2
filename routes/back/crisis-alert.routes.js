2162b1098f48c313a1d3866478878a7f6c958b2f
edquino <edquinosanchez@gmail.com>
2021-05-31 08:57:54 -0600

    Rutas Post y Put en Alerta Crisis

--- routes/back/crisis-alert.routes.js
+++ routes/back/crisis-alert.routes.js
@@ -2,11 +2,15 @@ const {Router} = require('express');
 const router = Router();
 
 const { usersTokenVerification } = require('@middlewares/token.middleware');
-const { getCrisisAlertsForm, crisisAlertsList,getById } = require('@controllers/back/crisis-alert.controllers');
+const { getCrisisAlertsForm, crisisAlertsList, getById, createCrisisAlert, updateCrisisAlert } = require('@controllers/back/crisis-alert.controllers');
 
 router.get('/api/crisis-alerts/list', crisisAlertsList);
 router.get('/api/crisis-alerts/form', getCrisisAlertsForm);
-router.get('/api/crisis-alerts/:id_atencion_crisis', getById);
+router.get('/api/crisis-alerts/:id_atencion_crisis', usersTokenVerification, getById);
+
+router.put('/api/early-alerts/:id_atencion_crisis', usersTokenVerification, updateCrisisAlert);
+
+router.post('/api/crisis-alerts/form', usersTokenVerification, createCrisisAlert);
 
 
 module.exports = router;
