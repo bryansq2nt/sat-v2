36b8de2ad4dd8b5cab9b8ace5c8b36374127ceda
Bryan Murgas <bryan_murgasvasquez@hotmail.com>
2021-06-10 12:28:14 -0600

    user token added to routes

--- routes/back/crisis-alert.routes.js
+++ routes/back/crisis-alert.routes.js
@@ -4,15 +4,15 @@ const router = Router();
 const { usersTokenVerification } = require('@middlewares/token.middleware');
 const { getCrisisAlertsForm, crisisAlertsList, getById, createCrisisAlert, updateCrisisAlert, getFormToAnalyze, analyzeCrisisAlert, searchCrisisAlert } = require('@controllers/back/crisis-alert.controllers');
 
-router.get('/api/crisis', crisisAlertsList);
+router.get('/api/crisis', usersTokenVerification,  crisisAlertsList);
 router.post('/api/crisis', usersTokenVerification, createCrisisAlert);
 
-router.get('/api/crisis/search', searchCrisisAlert);
+router.get('/api/crisis/search', usersTokenVerification, searchCrisisAlert);
 router.get('/api/crisis/:id_atencion_crisis', usersTokenVerification, getById);
 router.put('/api/crisis/:id_atencion_crisis', usersTokenVerification, updateCrisisAlert);
 
-router.get('/api/crisis/form/empty', getCrisisAlertsForm);
-router.get('/api/crisis/form/analyze/:id_atencion_crisis', getFormToAnalyze);
+router.get('/api/crisis/form/empty', usersTokenVerification, getCrisisAlertsForm);
+router.get('/api/crisis/form/analyze/:id_atencion_crisis', usersTokenVerification, getFormToAnalyze);
 router.put('/api/crisis/form/analyze/:id_atencion_crisis', usersTokenVerification, analyzeCrisisAlert);
 
 
--- routes/back/early-alerts.routes.js
+++ routes/back/early-alerts.routes.js
@@ -5,14 +5,14 @@ const { usersTokenVerification } = require('@middlewares/token.middleware');
 const { earlyAlertsList, getById, getEarlyAlertForm, createEarlyAlert, updateEarlyAlert, getFormToAnalyze, analyzeEarlyAlert, searchEarlyAlert } = require('@controllers/back/early-alert.controllers');
 
 router.post('/api/alerts', usersTokenVerification, createEarlyAlert);
-router.get('/api/alerts', earlyAlertsList);
+router.get('/api/alerts', usersTokenVerification, earlyAlertsList);
 
-router.get('/api/alerts/search', searchEarlyAlert);
-router.get('/api/alerts/:id_alerta_temprana',usersTokenVerification, getById);
-router.put('/api/alerts/:id_alerta_temprana',usersTokenVerification, updateEarlyAlert);
+router.get('/api/alerts/search', usersTokenVerification, searchEarlyAlert);
+router.get('/api/alerts/:id_alerta_temprana', usersTokenVerification, getById);
+router.put('/api/alerts/:id_alerta_temprana', usersTokenVerification, updateEarlyAlert);
 
-router.get('/api/alerts/form/empty', getEarlyAlertForm);
-router.get('/api/alerts/form/analyze/:id_alerta_temprana', getFormToAnalyze);
+router.get('/api/alerts/form/empty',usersTokenVerification, getEarlyAlertForm);
+router.get('/api/alerts/form/analyze/:id_alerta_temprana',usersTokenVerification, getFormToAnalyze);
 router.put('/api/alerts/form/analyze/:id_alerta_temprana',usersTokenVerification, analyzeEarlyAlert);
 
 
