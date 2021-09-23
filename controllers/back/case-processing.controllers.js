4eaae36f560421a93aa6a7fda5afe55f1f52cc37
edquino <edquinosanchez@gmail.com>
2021-09-23 15:44:33 -0600

    modificacion en controladores de tramitacion de casos

--- controllers/back/case-processing.controllers.js
+++ controllers/back/case-processing.controllers.js
@@ -213,7 +213,7 @@ let createCaseProcessing = async(req, res) => {
 
     var errorResponse = new ErrorModel({ type: "Case-Processing", title: "Falló la función", status: 500, detail: "Lo sentimos ocurrió un error al guardar el tramite del caso.", instance: "case-processing/createCaseProcessing" });
     
-    var numero_caso = 103;
+    var numero_caso = 113;
     // Datos no identificados de que tablas obtenerlos
     // cod_depto_ing
     // id_ins_ing
@@ -230,12 +230,12 @@ let createCaseProcessing = async(req, res) => {
     var user_name = 'Usuario Prueba';
 
     await db.query(`INSERT INTO tcdh_caso_temp(
-      id_caso_temp, hay_mas_vic_den, reg_ing_turno, en_turno, 
+      hay_mas_vic_den, reg_ing_turno, en_turno, 
       fec_en_turno, tipo_via_entrada, via_entrada, otra_via_entrada, id_usu_asignado, fec_asignado, fecha, fec_hora,  
       fec_hor_hecho_aprox, est_reg, fec_est_reg, cod_usu_ing, usu_ing_reg, fec_ing_reg, cod_usu_mod, usu_mod_reg, fec_mod_reg, 
       fuente, fec_emision, tit_emision, fec_recepcion, hor_recepcion)
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25)`, 
-          [numero_caso, 'N', 'N', 'N', registration_date, tipo_via_entrada, via_entrada, otra_via_entrada, cod_usu, fecha, 
+          ['N', 'N', 'N', registration_date, tipo_via_entrada, via_entrada, otra_via_entrada, cod_usu, registration_date, fecha, 
           registration_date, fec_hor_hecho_aprox, est_reg, registration_date, cod_usu, user_name, registration_date, cod_usu, 
           user_name, registration_date, fuente, fec_emision, tit_emision, fec_recepcion, hor_recepcion], (err, results)=>{
           if(err){
